5cfb73beb10cad6ab3c3e94a5bfba2959be94366
Dieres <matoshurtadodiegoaquiles@gmail.com>
2023-06-29 11:25:50 -0500

    last configs

--- app/[route]/page.tsx
+++ app/[route]/page.tsx
@@ -1,9 +1,8 @@
 import FilesContainer from '@/components/Content/FilesContainer'
 import Header from '@/components/Header/Header'
 import Modal from '@/components/Modal'
-import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
 
-export default function RoutePage({params} : Params) {
+export default function RoutePage({params} : {params : {route : string}}) {
   const {route} = params
   return (
     <main className='h-screen flex flex-col gap-4 bg-[rgb(3,0,28)] '>
